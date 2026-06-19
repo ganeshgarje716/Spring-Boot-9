e7731522d50c21c878ca5a2af08dbbf22a8e4a9a
Ganesh <ganeshgarje716@gmail.com>
2026-06-19 19:28:12 +0530

    Create welcome() and productForm() Methods

--- SpringBoot-9/src/main/java/com/ganesh/controller/WelcomeController.java
+++ SpringBoot-9/src/main/java/com/ganesh/controller/WelcomeController.java
@@ -1,5 +1,38 @@
 package com.ganesh.controller;
 
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.ganesh.entity.Products;
+
+
+@Controller
 public class WelcomeController {
+	
+	
+	@GetMapping({"/welcome","/home"})
+	public String welcome(Model model) {
+		
+		model.addAttribute("msg", "Good Morning");
+		model.addAttribute("names", List.of("Ganesh", "Ram", "Shyam", "Hari"));
+		
+		return "welcome";
+	}
+	
+	
+	@GetMapping("/product-form")
+	public String productForm(Model model) {
+		
+		Products product=new Products();
+		product.setName("Ganesh");
+		
+		model.addAttribute("product", product);
+		
+		
+		return "product-form";
+	}
 
 }
